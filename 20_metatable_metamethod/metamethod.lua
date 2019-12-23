7785fbec6897e76507b095aa1947023bb95a49a7
demon90s <demon90s@163.com>
2019-12-23 17:00:14 +0800

    modify metamethod

--- 20_metatable_metamethod/metamethod.lua
+++ 20_metatable_metamethod/metamethod.lua
@@ -23,10 +23,13 @@
     
     其他：
     __concat        定义连接运算符的行为
+    __tostring      默认的 tostring 方法，比如print函数首先就会调用元表的该方法，如果存在的话
+    ____metatable   保护元表，设置之后，不能再修改元表为其他元表
 ]]
 
 local Set = {}
 local mt = {}
+mt.__metatable = "none of your business"
 
 -- 创建一个Set
 function Set.new(l)
@@ -59,6 +62,7 @@ end
 
 mt.__mul = Set.intersection
 
+-- tostring
 function Set.tostring(set)
     local l = {}
     for e in pairs(set) do
@@ -67,6 +71,8 @@ function Set.tostring(set)
 
     return "{" .. table.concat(l, ',') .. "}"
 end
+
+mt.__tostring = Set.tostring
 
 ---test below
 
@@ -77,4 +83,7 @@ local s_union = s1 + s2
 print(Set.tostring(s_union))    --> 1,2,3,4,5,6,7
 
 local s_inter = s1 * s2
-print(Set.tostring(s_inter))    --> 3,4,5 (不一定按顺序)
+--print(Set.tostring(s_inter))    --> 3,4,5 (不一定按顺序)
+print(s_inter)
+
+--setmetatable(s1, {}) --> cannot change a protected metatable
