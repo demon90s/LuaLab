242cb67bc9c304867479d2d1db5f44cfb9717dee
demon90s <demon90s@163.com>
2019-12-30 21:35:49 +0800

    module

---
+++ 17_module/module_example.lua
@@ -0,0 +1,19 @@
+local Math = {}
+
+function Math.Add(a, b)
+    return a + b
+end
+
+function Math.Sub(a, b)
+    return a - b
+end
+
+function Math.Mul(a, b)
+    return a * b
+end
+
+function Math.Div(a, b)
+    return a / b
+end
+
+return Math
---
+++ 17_module/module_use.lua
@@ -0,0 +1,6 @@
+local m = require("module_example")
+
+print(m.Add(3, 4))          --> 7
+print(m.Sub(3, 4))          --> -1
+print(m.Mul(3, 4))          --> 12
+print(m.Div(3, 4))          --> 0.75
