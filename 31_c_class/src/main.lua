a08b75facacf7d25b6a249b89992585a05aad896
diwen <demon90s@163.com>
2020-02-21 11:35:21 +0800

    commit

--- 31_c_class/src/main.lua
+++ 31_c_class/src/main.lua
@@ -3,5 +3,6 @@ local vector = require "libvector"
 local vec = vector.new(10)
 
 print(vector.size(vec))
+--print(vector.size(io.stdin))  --> bad argument #1 to 'size' (libvectorint expected, got FILE*)
 
 vector.free(vec)
--- 31_c_class/src/vector.cpp
+++ 31_c_class/src/vector.cpp
@@ -4,6 +4,8 @@
 #include <cstdlib>
 #include <cstring>
 
+#define VECTOR_METATABLE "libvectorint"
+
 class VectorInt
 {
 public:
@@ -26,6 +28,10 @@ static int vector_new(lua_State *L)
 
     *vec_int = VectorInt(sz);
 
+    // 设置元表
+    luaL_getmetatable(L, VECTOR_METATABLE);  // 元表压栈
+    lua_setmetatable(L, -2);            // 为userdata设置元表,弹出元表
+
     return 1;   // 返回userdata
 }
 
@@ -34,8 +40,7 @@ static int vector_free(lua_State *L)
 {
     luaL_checktype(L, 1, LUA_TUSERDATA);
 
-    auto vec_int = static_cast<VectorInt*>(lua_touserdata(L, 1));
-    luaL_argcheck(L, vec_int != NULL, 1, "op NULL pointer");
+    auto vec_int = static_cast<VectorInt*>(luaL_checkudata(L, 1, VECTOR_METATABLE));
 
     vec_int->~VectorInt();
     // userdata内存不用释放,lua处理
@@ -48,8 +53,7 @@ static int vector_size(lua_State *L)
 {
     luaL_checktype(L, 1, LUA_TUSERDATA);
 
-    auto vec_int = static_cast<VectorInt*>(lua_touserdata(L, 1));
-    luaL_argcheck(L, vec_int != NULL, 1, "op NULL pointer");
+    auto vec_int = static_cast<VectorInt*>(luaL_checkudata(L, 1, VECTOR_METATABLE));
 
     lua_pushinteger(L, vec_int->m_v.size());
     
@@ -66,6 +70,10 @@ extern "C" {
     };
 
     int luaopen_libvector(lua_State *L) {
+
+        // 注册一个元表,用来标识这个类型
+        luaL_newmetatable(L, VECTOR_METATABLE);
+
         luaL_newlib(L, vector); // 创建一个表
         return 1;   // 把表返回给Lua(解释器)
     }
